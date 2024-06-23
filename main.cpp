2a5126c847d4407b753e9369e251f3ce2d706b11
vorobyevd <sparrow28048@gmail.com>
2024-06-23 12:23:41 +0300

    Add main.cpp and CMakeLists.txt

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+int main()
+{
+    double f1 = 0;
+    double f2 = 1;
+    double f3 = f1+f2;
+    double f4 = f3+f2;
+    double f5 = f4+f3;
+
+    std::cout<<"Fiboanacchi number 1: "<< f1 <<std::endl;
+    std::cout<<"Fiboanacchi number 2: "<< f2 <<std::endl;
+    std::cout<<"Fiboanacchi number 3: "<< f3 <<std::endl;
+    std::cout<<"Fiboanacchi number 4: "<< f4 <<std::endl;
+    std::cout<<"Fiboanacchi number 5: "<< f5 <<std::endl;
+        
+    return 0;
+}
+
+
