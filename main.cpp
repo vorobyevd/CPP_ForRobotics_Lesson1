54d48e53b9cea189908b73a9a53ef397a644a6ff
vorobyevd <sparrow28048@gmail.com>
2024-06-23 12:48:25 +0300

    Add commentaries to source files

--- main.cpp
+++ main.cpp
@@ -3,19 +3,21 @@
 
 int main()
 {
+    // Define variables and calculate fib numbers
     double f1 = 0;
     double f2 = 1;
     double f3 = f1+f2;
     double f4 = f3+f2;
     double f5 = f4+f3;
 
+    // Output results to console
     std::cout<<"Fiboanacchi number 1: "<< f1 <<std::endl;
     std::cout<<"Fiboanacchi number 2: "<< f2 <<std::endl;
     std::cout<<"Fiboanacchi number 3: "<< f3 <<std::endl;
     std::cout<<"Fiboanacchi number 4: "<< f4 <<std::endl;
     std::cout<<"Fiboanacchi number 5: "<< f5 <<std::endl;
     
-    
+
     return 0;
 }
 
