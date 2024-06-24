81289a55f46464e64502f1ae24f2719c575ab973
vorobyevd <sparrow28048@gmail.com>
2024-06-24 18:57:08 +0300

    Update main.cpp
    initial commit to branch 2

--- main.cpp
+++ main.cpp
@@ -1,24 +1,37 @@
 #include <iostream>
+#include <string>
 
 
 int main()
 {
-    // Define variables and calculate fib numbers
-    double f1 = 0;
-    double f2 = 1;
-    double f3 = f1+f2;
-    double f4 = f3+f2;
-    double f5 = f4+f3;
+  
+    int array[3][5] = {0};
 
-    // Output results to console
-    std::cout<<"Fiboanacchi number 1: "<< f1 <<std::endl;
-    std::cout<<"Fiboanacchi number 2: "<< f2 <<std::endl;
-    std::cout<<"Fiboanacchi number 3: "<< f3 <<std::endl;
-    std::cout<<"Fiboanacchi number 4: "<< f4 <<std::endl;
-    std::cout<<"Fiboanacchi number 5: "<< f5 <<std::endl;
+    for(int row = 0; row<3; ++row)
+    {
+        // for(int col = 0; col < 5; ++col)
+        // {
+        
+        //     std::cout << array[row][col] << ' '; //<< std::endl;
+
+        // }
+        for(auto number : array[row])
+        {
+            std::cout << number << ' '; //<< std::endl;
+
+        }
+        std::cout << std::endl;
+
+    }
+
+    char mystring[] = "string";
     
+    // for (auto sym:mystring)
+    // {
+    //     std::cout << sym;
+    // }
 
-    return 0;
+    // return 0;
 }
 
 
